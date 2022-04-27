6258fcd8e74bc2f26eefc7307a5fbd4ee9a982b6
JivinSardine <89859763+JivinSardine@users.noreply.github.com>
2022-04-27 19:38:49 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,20 +1,18 @@
 class Game {
   constructor() {}
-  //BP
+
   getState() {
     var gameStateRef = database.ref("gameState");
     gameStateRef.on("value", function(data) {
       gameState = data.val();
     });
   }
-  //BP
   update(state) {
     database.ref("/").update({
       gameState: state
     });
   }
 
-  // TA
   start() {
     player = new Player();
     playerCount = player.getCount();
@@ -31,20 +29,44 @@ class Game {
     car2.scale = 0.07;
 
     cars = [car1, car2];
+
+    // C38 TA
+    fuels = new Group();
+    powerCoins = new Group();
+
+    // Adding fuel sprite in the game
+    this.addSprites(fuels, 4, fuelImage, 0.02);
+
+    // Adding coin sprite in the game
+    this.addSprites(powerCoins, 18, powerCoinImage, 0.09);
   }
 
-  //BP
+  // C38 TA
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+      x = random(width / 2 + 150, width / 2 - 150);
+      y = random(-height * 4.5, height - 400);
+
+      var sprite = createSprite(x, y);
+      sprite.addImage("sprite", spriteImage);
+
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
   handleElements() {
     form.hide();
     form.titleImg.position(40, 50);
     form.titleImg.class("gameTitleAfterEffect");
   }
 
-  //SA
   play() {
     this.handleElements();
 
-    Player.getPlayersInfo(); //added
+    Player.getPlayersInfo();
 
     if (allPlayers !== undefined) {
       image(track, 0, -height * 5, width, height * 6);
@@ -52,20 +74,25 @@ class Game {
       //index of the array
       var index = 0;
       for (var plr in allPlayers) {
+        //add 1 to the index for every loop
+        index = index + 1;
+
         //use data form the database to display the cars in x and y direction
         var x = allPlayers[plr].positionX;
         var y = height - allPlayers[plr].positionY;
 
-        cars[index].position.x = x;
-        cars[index].position.y = y;
+        cars[index - 1].position.x = x;
+        cars[index - 1].position.y = y;
 
-        //add 1 to the index for every loop
-        index = index + 1;
-
+        // C38  SA
         if (index === player.index) {
           stroke(10);
           fill("red");
           ellipse(x, y, 60, 60);
+
+          this.handleFuel(index);
+          this.handlePowerCoins(index);
+
         }
       }
 
@@ -74,8 +101,28 @@ class Game {
         player.positionY += 10;
         player.update();
       }
- 
+
       drawSprites();
     }
+  }
+
+  handleFuel(index) {
+    // Adding fuel
+    cars[index - 1].overlap(fuels, function(collector, collected) {
+      player.fuel = 185;
+      //collected is the sprite in the group collectibles that triggered
+      //the event
+      collected.remove();
+    });
+  }
+
+  handlePowerCoins(index) {
+    cars[index - 1].overlap(powerCoins, function(collector, collected) {
+      player.score += 21;
+      player.update();
+      //collected is the sprite in the group collectibles that triggered
+      //the event
+      collected.remove();
+    });
   }
 }
